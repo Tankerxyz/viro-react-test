a54d336ee184ead39060a4269af2a18edbdb3015
Tankerxyz <tankerxyz1@gmail.com>
2018-03-12 19:03:59 +0200

    init commit

---
+++ js/HelloWorldSceneAR.js
@@ -0,0 +1,61 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {StyleSheet} from 'react-native';
+
+import {
+  ViroARScene,
+  ViroText,
+  ViroBox,
+  ViroMaterials,
+} from 'react-viro';
+
+export default class HelloWorldSceneAR extends Component {
+
+  constructor() {
+    super();
+
+    // Set initial state here
+    this.state = {
+      text : "Initializing AR..."
+    };
+
+    // bind 'this' to functions
+    this._onInitialized = this._onInitialized.bind(this);
+  }
+
+  render() {
+    return (
+      <ViroARScene onTrackingInitialized={this._onInitialized} >
+        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+        <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} materials={["grid"]} />
+      </ViroARScene>
+    );
+  }
+
+  _onInitialized() {
+    this.setState({
+      text : "Zdarova! openGeeksLab"
+    });
+  }
+
+}
+
+ViroMaterials.createMaterials({
+  grid: {
+    diffuseTexture: require('./res/grid_bg.jpg'),
+  },
+});
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',  
+  },
+});
+
+module.exports = HelloWorldSceneAR;
