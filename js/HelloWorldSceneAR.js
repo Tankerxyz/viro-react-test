1a7fba265ec9d313b8db5527df03928c85ea7bb6
Viktor <drudeveldvurech@gmail.com>
2018-03-19 08:47:04 +0200

    add character with default animation

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -1,75 +1,131 @@
-'use strict';
-
 import React, { Component } from 'react';
 
 import {StyleSheet} from 'react-native';
 
 import {
+  ViroSceneNavigator,
   ViroARScene,
-  ViroText,
-  ViroBox,
   ViroMaterials,
-  ViroARImageMarker,
-  ViroARTrackingTargets,
+  ViroAmbientLight,
+  ViroSpotLight,
+  ViroDirectionalLight,
+  ViroAnimations,
+  ViroParticleEmitter,
+  ViroSurface,
+  Viro3DObject,
+  ViroNode,
 } from 'react-viro';
 
-export default class HelloWorldSceneAR extends Component {
+var createReactClass = require('create-react-class');
+var MainScene = createReactClass({
+  getInitialState() {
+    return {
+      runAnimation:true,
+    }
+  },
 
-  constructor() {
-    super();
-
-    // Set initial state here
-    this.state = {
-      text : "Initializing AR..."
-    };
-
-    // bind 'this' to functions
-    this._onInitialized = this._onInitialized.bind(this);
-  }
-
-  render() {
+  render: function() {
     return (
-      <ViroARScene onTrackingInitialized={this._onInitialized} >
-        <ViroARImageMarker
-          target={"targetOne"}
-          onAnchorFound={() => {console.log('found')}}
-          opacity={0.1} >
-          <ViroBox position={[0, .25, 0]} scale={[2, 2, 2]} />
-        </ViroARImageMarker>
+      <ViroARScene>
+        <ViroAmbientLight color="#ffffff" intensity={200}/>
+
+        <ViroNode position={[0, -1, -2]} dragType="FixedToWorld" onDrag={()=>{}}>
+          <ViroSpotLight
+            innerAngle={5}
+            outerAngle={25}
+            direction={[0,-1,0]}
+            position={[0, 5, 0]}
+            color="#ffffff"
+            castsShadow={true}
+            shadowMapSize={2048}
+            shadowNearZ={2}
+            shadowFarZ={7}
+            shadowOpacity={.7}
+          />
+
+          <Viro3DObject
+            source={require('./res/icecreamman_anim/icecreamman_anim_a.vrx')}
+            resources={[require('./res/icecreamman_anim/icecreamman_diffuse.png'),
+              require('./res/icecreamman_anim/icecreamman_normal.png'),
+              require('./res/icecreamman_anim/icecreamman_specular.png')]}
+            position={[0, 0, 0]}
+            scale={[.5, .5, .5]}
+            type="VRX"
+            onClick={this._onTappedIcecream}
+            animation={{name:"02", run:this.state.runAnimation, loop:true,}}
+          />
+
+          <ViroSurface
+            rotation={[-90, 0, 0]}
+            position={[0, -.001, 0]}
+            width={2.5} height={2.5}
+            arShadowReceiver={true}
+          />
+        </ViroNode>
+
+        <ViroParticleEmitter
+          position={[0, 4.5, 0]}
+          duration={2000}
+          visible={true}
+          run={true}
+          loop={true}
+          fixedToEmitter={true}
+
+          image={{
+            source:require("./res/particle_snow.png"),
+            height:0.01,
+            width:0.01,
+            bloomThreshold:1.0
+          }}
+
+          spawnBehavior={{
+            particleLifetime:[5000,5000],
+            emissionRatePerSecond:[200,200],
+            spawnVolume:{
+              shape:"box",
+              params:[20, 1, 20],
+              spawnOnSurface:false
+            },
+            maxParticles:2000
+          }}
+
+          particleAppearance={{
+            opacity:{
+              initialRange:[0,0],
+              interpolation:[
+                {endValue:1.0, interval:[0,500]},
+                {endValue:0.0, interval:[4000,5000]}
+              ]
+            },
+            scale:{
+              initialRange:[[5,5,5], [10,10,10]],
+              interpolation:[
+                {endValue:[6,6,6], interval:[0,1000]},
+                {endValue:[10,10,10], interval:[3000,5000]},
+                {endValue:[5,5,5], interval:[4000,5000]}
+              ]
+            }
+          }}
+
+          particlePhysics={{
+            velocity:{
+              initialRange:[
+                [-2,-.5,0],
+                [2,-3.0,0]
+              ]
+            }
+          }}
+        />
+
       </ViroARScene>
     );
-  }
+  },
 
-  _onInitialized() {
+  _onTappedIcecream() {
     this.setState({
-      text : "Zdarova! openGeeksLab"
-    });
-  }
-
-}
-
-ViroMaterials.createMaterials({
-  grid: {
-    diffuseTexture: require('./res/grid_bg.jpg'),
+      runAnimation : !this.state.runAnimation,
+    })
   },
 });
 
-ViroARTrackingTargets.createTargets({
-  "targetOne" : {
-    source : require('./res/qr.jpg'),
-    orientation : "Down",
-    physicalWidth : 0.07 // real world width in meters
-  },
-});
-
-var styles = StyleSheet.create({
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',  
-  },
-});
-
-module.exports = HelloWorldSceneAR;
+module.exports = MainScene;
